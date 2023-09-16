73d87d79e09427ce4a73cf62eb61e4a47b9967c1
VictorVillazante <villazante.bernabe.victor@gmail.com>
2023-09-16 15:25:02 -0400

    agregando proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Mascota} from './modelos/mascota';
+//import { from } from 'rxjs';
+import{Sexo} from './modelos/sexo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-refugio';
+  mascotaArray: Mascota[]=[
+    {id:1,nombre:"Mora",sexo:"Hembra",raza:"Yorkie",edad:3},
+    {id:2,nombre:"Bethoveen",sexo:"Macho",raza:"San Bernardo",edad:5},
+    {id:3,nombre:"Wayra",sexo:"Hembra",raza:"Labrador",edad:6},
+
+  ];
+  sexoArray: Sexo[]=[
+    {id:1,sexo:"Macho"},
+    {id:2,sexo:"Hembra"},
+
+  ];
+  selectedMascota: Mascota = new Mascota();
+  addOrEdit(){
+    if(this.selectedMascota.id==0){
+      this.selectedMascota.id=this.mascotaArray.length+1;
+      this.mascotaArray.push(this.selectedMascota);
+    }
+    this.selectedMascota = new Mascota();
+   
+  }
+  registrarpet(){
+    this.editar=false;
+    this.selectedMascota = new Mascota();
+  }
+  editar:boolean=false;
+  editPet(mascota:Mascota){
+    this.editar=true;
+    console.log("edit pet");
+    this.openForEdit(mascota);
+  }
+  openForEdit(mascota: Mascota){
+    this.selectedMascota=mascota;
+    console.log(this.selectedMascota)
+  }
+  RowSelected(mascota: Mascota){
+    this.selectedMascota=mascota;
+  }
+  deletePet(selectedMascota: Mascota){
+    if(confirm('Está seguro que desea eliminar a la mascota?')){
+      this.mascotaArray=this.mascotaArray.filter(x=> x!=selectedMascota);
+    }
+
+  }
+  cargandoImagen(event){
+    console.log(event);
+  }
 }
---
+++ src/app/modelos/mascota.ts
@@ -0,0 +1,9 @@
+export class Mascota {
+    id:number=0;
+    nombre: string;
+    sexo: string;
+    raza: string;
+    edad: number;
+    //foto: string;
+
+}
